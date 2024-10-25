d9c61796cb0c3170eb2bfe3f2604f9952afb74b7
Vladislav Pavlov (ISh-) <imwaves@gmail.com>
2024-10-25 07:17:39 +0200

    prerelease

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@
 #include <oscpp/print.hpp>
 #include "OscServer.hpp"
 
-#include "log.hpp"
+#include "math.cpp"
+#include "log.cpp"
+#include "StateChangeDetector.hpp"
 // #include "ogl.hpp"
 
 float FPS = 60.f;
@@ -20,20 +22,16 @@ void DrawRenderTexture (RenderTexture2D rt) {
     DrawTextureRec(rt.texture, {0,0,wSize.x,-wSize.y}, {0,0}, WHITE);
 }
 
-float mapRange(float x, float in_min, float in_max, float out_min, float out_max) {
-    return out_min + (x - in_min) * (out_max - out_min) / (in_max - in_min);
-}
-
-float lag(float current, float target, float smoothingFactor, float dt) {
-    return current + (target - current) * smoothingFactor * dt;
-}
-
 class Pointer {
 public:
     Vector2 pos {0, 0};
     float speed = 0.;
+    Vector2 _targetPos;
+
+    Pointer (Vector2 pos) : pos(pos) {}
 
     void update (float dt, Vector2 targetPos) {
+        _targetPos = targetPos;
         Vector2 nextPos {
             lag(pos.x, targetPos.x, 3., dt),
             lag(pos.y, targetPos.y, 3., dt) };
@@ -50,14 +48,17 @@ int main()
 {
     OscServer server(3337);
     InitWindow(wSize.x, wSize.y, "Osc Cpp");
+    ToggleBorderlessWindowed();
+    HideCursor();
+    DisableCursor();
     wSize = {(float)GetMonitorWidth(0), (float)GetMonitorHeight(0)};
     printf("WINDOW SIZE: %f - %f", wSize.x, wSize.y);
-    // ToggleBorderlessWindowed();
     SetWindowSize(wSize.x, wSize.y);
+
     // SetTargetFPS(FPS);
     ClearBackground(BLACK);
 
-    Vector2 mouse {0,0};
+    Vector2 mouse {wSize.x/2,wSize.y/2};
     int mouseZ = 0;
     int frame = 0;
 
@@ -86,15 +87,13 @@ int main()
     if (!IsShaderReady(opacityShader))
         throw std::runtime_error("opacity Shader not ready");
 
-    // DrawTextureRec(Texture2D texture, Rectangle source, Vector2 position, Color tint)
-
-    Pointer pointer;
-
-    // using FuncType = std::function<void(float)>;
-    // std::unordered_map<std::string, FuncType> chanToFunc {
-    //     {"/leftWrist_x", [&pointer](float val) { pointer.x = val; }},
-    //     {"/leftWrist_y", [&pointer](float val) { pointer.y = val; }},
-    // };
+    Pointer pointer ({ wSize.x/2, wSize.y/2 });
+    StateChangeDetector fullscreenBtn;
+    StateChangeDetector mouseModeBtn;
+    bool mouseMode;
+    StateChangeDetector showDebugBtn;
+    bool showDebug;
+    bool oscGotMessage = false;
 
     std::unordered_map<std::string, float> oscChanVals;
 
@@ -102,49 +101,49 @@ int main()
     while (!WindowShouldClose()) {
         double now = GetTime(); frame++;
         double delta = GetFrameTime();
-        mouse = GetMousePosition();
-        mouseZ = IsMouseButtonPressed(0) ? 1 : (IsMouseButtonPressed(1) ? -1 : 0);
+        if (mouseMode) {
+            mouse = GetMousePosition();
+            mouseZ = IsMouseButtonPressed(0) ? 1 : (IsMouseButtonPressed(1) ? -1 : 0);
+        }
+
+        if (fullscreenBtn.hasChangedOn(IsKeyPressed(KEY_F)))
+            ToggleBorderlessWindowed();
+        if (mouseModeBtn.hasChangedOn(IsKeyPressed(KEY_M)))
+            mouseMode = !mouseMode;
+        if (showDebugBtn.hasChangedOn(IsKeyPressed(KEY_D)))
+            showDebug = !showDebug;
 
         BeginDrawing();
             ClearBackground(BLACK);
 
             // try {
-                server.recv([&oscChanVals](OSCPP::Server::Message msg) {
+                server.recv([&oscChanVals, &oscGotMessage](OSCPP::Server::Message msg) {
+                    oscGotMessage = true;
                     OSCPP::Server::ArgStream args = msg.args();
                     std::string address = msg.address();
-                    // float v = args.float32();
-                    // if (msg == "/leftWrist_x")
-                    //     printf("Received: %s %f\n", address.c_str(), v);
-                    // print()
+
                     oscChanVals[address] = args.float32();
-                    // auto it = chanToFunc.find(address);
-                    // if (it != chanToFunc.end()) {
-                    //     FuncType func = it->second;
-                    //     func(args.float32());
-                    // } else if (true)
-                        // std::cout << "Received: " << msg << std::endl;
                 });
             // } catch (const std::exception& e) {
             //     std::cerr << e.what() << std::endl;
             // }
-
-            pointer.update(delta,
-                (Vector2) { oscChanVals["/leftWrist_x"], oscChanVals["/leftWrist_y"] });
-            printf("speed: %f\n", pointer.speed);
-            // printf("Pointer: %f, %f\n", pointer.pos.x, pointer.pos.y);
-            // printf("Pointer.target: %f, %f\n", pointer.targetPos.x, pointer.targetPos.y);
+            Vector2 pointerTarget =  (mouseMode || !oscGotMessage) ? mouse
+                : (Vector2) {
+                    mapRange(oscChanVals["/leftWrist_x"], 0.5, 1.5, 0., wSize.x),
+                    mapRange(oscChanVals["/leftWrist_y"], 1., 3., wSize.y, 0.),
+                };
+            pointer.update(delta,pointerTarget);
 
             RenderTexture2D thisRT = feedbackCache[frame % feedbackCacheSize];
             RenderTexture2D prevRT = feedbackCache[(frame+1) % feedbackCacheSize];
 
             BeginTextureMode(thisRT);
-                // Color circleColor = ColorFromHSV(now * 30., 1., 1.);
-                Color circleColor = {255,0,0,255};
+                Color circleColor = {255,150,0,255};
+                float radius = (!mouseMode && !oscGotMessage) ? (!((int)(now*2) % 10) ? 40 : 0)
+                    : Clamp(mapRange(pointer.speed, 0, 18, 0, 40), 0, 80);
                 DrawCircle(
-                    mapRange(pointer.pos.x, 0.5, 1.5, 0., wSize.x),
-                    mapRange(pointer.pos.y, 1., 3., wSize.y, 0.),
-                    Clamp(mapRange(pointer.speed * 100., 0, 2, 0, 40), 0, 80),
-                    circleColor);
+                    pointer.pos.x, pointer.pos.y,
+                    radius, circleColor);
 
                 BeginShaderMode(edgeShader);
                     DrawRenderTexture(prevRT);
@@ -158,9 +157,12 @@ int main()
             EndTextureMode();
 
             DrawRenderTexture(thisRT);
-            // EndShaderMode();
+
             DrawText(TextFormat("%i fps", GetFPS()), 20, 20, 30, WHITE);
-            // DrawText(TextFormat("%i : %i", (int)mouse.x, (int)mouse.y), 20, 50, 30, BLACK);
+            if (showDebug) {
+                DrawText(TextFormat("pointer: %.0f : %.0f", pointer._targetPos.x, pointer._targetPos.y), 20, 50, 30, WHITE);
+                DrawText(TextFormat("speed: %.3f", pointer.speed), 20, 80, 30, WHITE);
+            }
         EndDrawing();
     }
 
